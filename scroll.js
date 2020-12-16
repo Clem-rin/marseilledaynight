9979f34ab6a80b0192655589315de363bbf21adb
Clem-rin <72383804+Clem-rin@users.noreply.github.com>
2020-12-16 11:06:33 +0100

    Add files via upload

---
+++ scroll.js
@@ -0,0 +1,33 @@
+const hours = document.getElementById('hours');
+const dayHeight = document.getElementById('sunset').offsetTop;
+const heightToNight = document.getElementById('soir').offsetTop;
+const afternoonHeight = heightToNight - dayHeight;
+/*const nightHeight = document.body.offsetHeight - dayHeight - afternoonHeight;*/
+hours.innerHTML="9h00";
+window.addEventListener('scroll', (e) => 
+{
+  let y = window.scrollY;
+  if (y <= dayHeight) {
+    heightHour = dayHeight / 6;
+    time = Math.trunc(y / heightHour) + 9;
+    hours.innerHTML = time + "h00";
+    console.log(time);
+  } 
+
+  else /*if(y>=dayHeight && y<=heightToNight)*/ {
+    heightHour = afternoonHeight/6;
+    time = Math.trunc(y / heightHour) + 13;
+    if (time > 23 ) { time = time - 24};
+    hours.innerHTML = time + "h00";
+  console.log(time);
+  }
+
+/*  else  
+  {
+    heightHour = nightHeight / 12;
+    time = Math.trunc(y / heightHour) + 6;
+    if (time > 23 ) { time = time - 24};
+    hours.innerHTML = time + "h00";
+  console.log(time);
+  }*/
+});
